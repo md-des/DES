97db3f747b5a0bf6ccb29497b05fd800e0e561cb
zlx <zlx@omk.io>
2019-01-28 21:39:25 +0800

    add icon

--- src/components/EditorOverview.js
+++ src/components/EditorOverview.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Spin } from 'antd';
+import { Spin, Icon } from 'antd';
 import Stackedit from 'stackedit-js';
 export default class EditorOverview extends Component {
   state = {}
@@ -28,15 +28,19 @@ export default class EditorOverview extends Component {
   }
   render() {
     const {loading} = this.state;
+    const {content} = this.props;
     return (
       <div>
         <div style={{width: '100%', maxWidth: '800px', textAlign: 'center'}}>
               <Spin spinning={loading === true}/>
         </div>
         <textarea style={{display: 'none'}} id="stackeditOverview" />
-        <div id="stackeditOverviewPanel" style={{background: loading && 'rgba(0,0,0,0.05)'}}>
-
-        </div>
+        <div id="stackeditOverviewPanel" style={{background: loading && 'rgba(0,0,0,0.05)'}} />
+        {!content &&
+          <div style={{width: '100%', maxWidth: '800px', textAlign: 'center'}}>
+          <Icon type="read" style={{fontSize: '50px'}}/>
+          </div>
+        }
       </div>
     )
   }
